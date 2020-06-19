0ad20935c82ad4fe9a09430e3d667a5c26ce6b2a
Andrey Sapranovich <SapranovichAndrey@yandex.ru>
2020-06-19 14:04:14 +0300

    feat:add modal window

---
+++ precache-manifest.8a62651d1bae69976f943ad6162b41ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed174f84b30d3e49608de31f64a0117",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3254bff4f6a4c938ff50",
+    "url": "/static/css/main.6ad9a50b.chunk.css"
+  },
+  {
+    "revision": "eea560e0524e9d7e5fb4",
+    "url": "/static/js/2.1b6e7c5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b6e7c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3254bff4f6a4c938ff50",
+    "url": "/static/js/main.582becfb.chunk.js"
+  },
+  {
+    "revision": "d69605dfe2f4bbf2ec86",
+    "url": "/static/js/runtime-main.59057f75.js"
+  }
+]);
